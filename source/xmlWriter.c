13f183ebf9c32d1bf6abbcf2d6d5245bfce291cc
Desmond <ds.janssen@student.han.nl>
2015-04-22 12:22:35 +0200

    xmlWriter toegevoegd om users op te slaan

---
+++ source/xmlWriter.c
@@ -0,0 +1,5 @@
+//
+// Created by desmond on 4/22/15.
+//
+
+#include "xmlWriter.h"
---
+++ source/xmlWriter.h
@@ -0,0 +1,8 @@
+//
+// Created by desmond on 4/22/15.
+//
+
+#ifndef DUDESERVER_XMLWRITER_H
+#define DUDESERVER_XMLWRITER_H
+
+#endif //DUDESERVER_XMLWRITER_H
