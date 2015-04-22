d96d31237737c3581028cc5e68393062a54833a9
Desmond <ds.janssen@student.han.nl>
2015-04-22 12:28:24 +0200

    xmlWriter content

--- source/xmlWriter.c
+++ source/xmlWriter.c
@@ -3,3 +3,38 @@
 //
 
 #include "xmlWriter.h"
+
+int writeUser(struct user user)
+{
+    printf("User %s wordt opgeslagen", user.username);
+    xmlTextWriterPtr xmlptr = openFile(user.username);
+    if(xmlptr == NULL)
+        return 1;
+    xmlTextWriterStartElement(xmlptr, "user");
+    xmlTextWriterWriteElement(xmlptr, "nickname", user.nickname);
+    xmlTextWriterWriteElement(xmlptr, "password", user.password);
+    writeChannels(xmlptr, user.channels);
+    xmlTextWriterEndElement(xmlptr);
+    xmlTextWriterFlush(xmlptr);
+    return 0;
+}
+
+xmlTextWriterPtr openFile(char *username) {
+    char filename[250];
+    sprintf(filename, "database/users/%s.xml", username);
+    return xmlNewTextWriterFilename(filename,0);
+}
+
+void writeChannels(xmlTextWriterPtr xmlptr, char **channels) {
+    xmlTextWriterStartElement(xmlptr, "channels");
+    while(channels != NULL && channels != 0)
+    {
+        if(*channels == NULL) {
+            channels++;
+            break;
+        }
+        xmlTextWriterWriteElement(xmlptr, "channel", *channels);
+        channels++;
+    }
+    xmlTextWriterEndElement(xmlptr);
+}
--- source/xmlWriter.h
+++ source/xmlWriter.h
@@ -2,7 +2,20 @@
 // Created by desmond on 4/22/15.
 //
 
+#include <libxml/xmlwriter.h>
+
 #ifndef DUDESERVER_XMLWRITER_H
 #define DUDESERVER_XMLWRITER_H
 
 #endif //DUDESERVER_XMLWRITER_H
+
+struct user {
+    char *nickname;
+    char *username;
+    char *password;
+    char **channels;
+};
+
+int writeUser(struct user user);
+xmlTextWriterPtr openFile(char *username);
+void writeChannels(xmlTextWriterPtr xmlptr, char **channels);
