b83b9f4e36809f359afbb4c8e68a94123935be21
fatihict <f.demir@student.han.nl>
2015-04-29 10:47:34 +0200

    Update user.h

--- source/database/user.h
+++ source/database/user.h
@@ -1,22 +1,11 @@
-//
-// Created by osboxes on 28/04/15.
-//
-
 #ifndef DUDESERVER_USER_H
 #define DUDESERVER_USER_H
 
-#endif //DUDESERVER_USER_H
-
-#ifndef DUDESERVER_DATABASE_H
 #include "database.h"
-#endif //DUDESERVER_DATABASE_H
-
-
 
 int checkUser(char *userName);
-
 char **getUserList();
-
 int getUser(char *username, userInfo *result);
-
 void userJoinChannel(char *username, char *channelName);
+
+#endif
