a53055ea903325698648c2def944b79faab845c9
Sjuul Wijnia <sjuulwijnia@gmail.com>
2015-05-29 11:32:22 +0200

    Merge branch 'memoryhunt' into develop
    Conflicts:
    	test/main.c

--- source/ssl/communication.c
+++ source/ssl/communication.c
@@ -164,7 +164,7 @@ int sslSend(char* snd)
         return SSL_NO_CONNECTION;
     }
 
-    char* buffer = malloc(sizeof(char) * bufferLength + 2);
+    char* buffer = malloc(sizeof(char) * (bufferLength + 2));
     sprintf(buffer, "%s\r\n", snd);
     int written = SSL_write(connection.ssl_handle, buffer, (int)(bufferLength + 2));
     free(buffer);
