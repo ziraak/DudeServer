2cd2ae2aef7dd1599635e6ef4e9ecb60ca0ee933
Fatih Demir <f.demir@student.han.nl>
2015-06-01 11:26:41 +0200

    Role toegevoegd aan het names commando.

--- source/commands/names.c
+++ source/commands/names.c
@@ -17,15 +17,34 @@ int handleNamesCommand(commandStruct cmd)
 
     channelUserStruct = getUsersFromChannel(channelName);
 
-    char *users = "";
+    size_t mallocUsersLength = 0;
+    size_t mallocRolesLength = 0;
     int i = 0;
     while (channelUserStruct[i].username != NULL)
     {
-        strcat(users, " ");
-        strcat(users, channelUserStruct[i].username);
+        mallocUsersLength += strlen(channelUserStruct[i].username) + 6 + 1 + 1; // 6 = "REPLY " AND 1 = "," AND 1 = " "
+        mallocRolesLength += strlen(channelUserStruct[i].role) + 1; // 1 = ","
         i++;
     }
+
+    char *users = malloc(mallocUsersLength + mallocRolesLength);
+    char *roles = malloc(mallocRolesLength);
+    strcat(users, "REPLY ");
+    strcat(roles, "");
+    i = 0;
+    while (channelUserStruct[i].username != NULL)
+    {
+        strcat(users, ",");
+        strcat(users, channelUserStruct[i].username);
+        strcat(roles, ",");
+        strcat(roles, channelUserStruct[i].role);
+        i++;
+    }
+    strcat(users, " ");
+    strcat(users, roles);
     sslSend(users);
+    free(users);
+    free(roles);
     channelUser_free(channelUserStruct);
     return RPL_SUCCESS;
 }
