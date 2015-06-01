9fefb38ef570a802cd17d7d987debf40656b652c
Sjuul Wijnia <sjuulwijnia@gmail.com>
2015-06-01 14:30:12 +0200

    Columns bij getChannelByName() verwijdert.

--- source/main.c
+++ source/main.c
@@ -3,18 +3,18 @@
 int main(int argc, char **argv)
 {
 
-    mallopt(M_CHECK_ACTION, 1);
-
-    int fork = BOOL_FALSE;
-    if(argc > 1)
-    {
-        if(strcmp(argv[1], "FORK") == 0 || strcmp(argv[1], "fork") == 0 || strcmp(argv[1], "-f") == 0)
-        {
-            fork = BOOL_TRUE;
-        }
-    }
-
-    runServer(fork, SERVER_PORT);
+//    mallopt(M_CHECK_ACTION, 1);
+//
+//    int fork = BOOL_FALSE;
+//    if(argc > 1)
+//    {
+//        if(strcmp(argv[1], "FORK") == 0 || strcmp(argv[1], "fork") == 0 || strcmp(argv[1], "-f") == 0)
+//        {
+//            fork = BOOL_TRUE;
+//        }
+//    }
+//
+//    runServer(fork, SERVER_PORT);
 
 
     if(setupDatabaseConnection() != DB_RETURN_SUCCES)
